562d75b2467afac982a874635338f64976b3356b
Spencer Moody <spencer.moody@outlook.com>
2020-03-24 15:57:34 -0700

    Add scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).click(function() {
   $("button#hello").click(function() {
     $("ul#user").prepend("<li>Hello!</li>");
     $("ul#webpage").prepend("<li>Why hello there!</li>");
@@ -12,5 +12,14 @@ $(document).ready(function() {
   $("button#stop").click(function() {
     $("ul#user").prepend("<li>Stop copying me!</li>");
     $("ul#webpage").prepend("<li>Pardon me. I meant no offense.</li>");
+
+    $("ul#user").children("li").first().click(function() {
+      alert('hi');
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      alert('hi');
+    });
   });
 });
+
+
